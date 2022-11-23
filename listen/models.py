ce5e10769c6bef8124ba10a9555a8ae9821c8b35
CZandt <95643051+CZandt@users.noreply.github.com>
2022-11-23 13:45:26 -0800

    Add files via upload
    Forked it and now created my own thing. added the models and template stuff

--- listen/models.py
+++ listen/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# create a model to store the song information
+
+class Song(models.Model):
+    title= models.CharField(max_length= 100)
+    artist= models.CharField(max_length= 50)
+    image= models.ImageField()
+    audio_file= models.FileField(blank=True, null=True)
+    duration = models.CharField(max_length=20)
+    paginate_by = 2
+
+    def __str__(self):
+        return self.title
+
+    
--- listen/views.py
+++ listen/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.core.paginator import Paginator
+from .models import Song
 from django.http import HttpResponse
 
-def listen(request):
-    return HttpResponse("This will be the listening page")
+def listenPageView(request):
+    #paginator = Paginator(Song.objects.all(), 1)
+    #page_number = request.GET.get('page')
+    #page_obj = paginator.get_page(page_number)
+    #context={'page_obj' : page_obj}
+    
+    return render(request,'listen/index.html') #FIX THIS TO USE THE ROUTES???
+
 
 def addSong(request):
     return HttpResponse("Song has been added")
 
 def addSkipVote(request):
     return HttpResponse("Skip vote has been added")
+
+
